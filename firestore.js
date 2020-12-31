97a8bb8049bec62b18a7eee49f9214de04b5bb55
wyattbarber <wyattjbarber@gmail.com>
2020-12-31 15:40:02 -0500

    Trying again

--- firestore.js
+++ firestore.js
@@ -1,22 +1,20 @@
 admin = require('firebase-admin');
 admin.initializeApp();
 const db = admin.firestore();
-const settings = {timestampsInSnapshots: true};
+const settings = { timestampsInSnapshots: true };
 db.settings(settings);
 
-/**
- * Asynchronously accesses the authentication document for the given ClientID
- * 
- * @param {!express:Request} req 
- * @param {!express:Response} res 
- */
-export async function getUserData(req, res){
-    const clientDoc = await db.collection('Home Data').doc(req.query.client_id).get();
-    if (clientDoc.exists) {
-        return clientDoc;
-    }
-    else {
-        res.status(404).send({error: 'Could not locate document for this client'});
-        throw 'Error opening document';
-    }
-}
+
+
+module.exports = function () {
+    getUserData: async.function(req, res) {
+        const clientDoc = await db.collection('Home Data').doc(req.query.client_id).get();
+        if (clientDoc.exists) {
+            return clientDoc;
+        }
+        else {
+            res.status(404).send({ error: 'Could not locate document for this client' });
+            throw 'Error opening document';
+        }
+    };
+};
