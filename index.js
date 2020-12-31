f6ea5805383cac6102557f5e6191bd9977656d7e
wyattbarber <wyattjbarber@gmail.com>
2020-12-31 17:59:36 -0500

    Added try catch to validation block

--- index.js
+++ index.js
@@ -19,21 +19,25 @@ exports.main = (req, res) => {
   }
 
   // Validate data sent in request
-  const uriOK = (req.query.redirect_uri === 'https://oauth-redirect.googleusercontent.com/r/myhome-5f414');
 
-  if (uriOK) {
-    let redirectAddr = req.query.redirect_uri;
-    redirectAddr += '#access_token=' + userData.AuthToken.toString();
-    redirectAddr += '&token_type=bearer';
-    redirectAddr += '&state=' + requestAnimationFrame.query.state.toString();
+  try {
+    const uriOK = (req.query.redirect_uri === 'https://oauth-redirect.googleusercontent.com/r/myhome-5f414');
 
-    res.redirect(redirectAddr);
+    if (uriOK) {
+      let redirectAddr = req.query.redirect_uri;
+      redirectAddr += '#access_token=' + userData.AuthToken.toString();
+      redirectAddr += '&token_type=bearer';
+      redirectAddr += '&state=' + requestAnimationFrame.query.state.toString();
+
+      res.redirect(redirectAddr);
+    }
+    else {
+      res.status(404).send({
+        error: 'Invalid client data: ' + req.query.client_id + ', ' + req.query.redirect_uri});
+    }
   }
-  else {
-    res.status(404).send({
-      error: 'Invalid client data: ' + req.query.client_id + ', ' + req.query.redirect_uri + '. '
-        + 'Firestore: ' + doc.toString() + ', ' + userData.toString()
-    });
+  catch (e) {
+    res.status(404).send({error: 'Exception thrown: '+e});
   }
 
 }
