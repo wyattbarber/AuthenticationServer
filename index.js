558fc5b6d594bfd32ad0aabbcddbcb31514c2212
wyattbarber <wyattjbarber@gmail.com>
2020-12-30 22:12:48 -0500

    Added try catch

--- index.js
+++ index.js
@@ -11,27 +11,35 @@ const firestore = new Firestore({
 });
 
 exports.main = (req, res) => {
-  // Load approved user data
-  const authDoc = firestore.collection('Home Data').doc('Auth').get();
-  if (!doc.exists()) {
-    res.status(404).send({error: 'Document not found'});
-    return;
+  try {
+    // Load approved user data
+    const authDoc = firestore.collection('Home Data').doc('Auth').get();
+    if (!doc.exists()) {
+      res.status(404).send({ error: 'Document not found' });
+      return;
+    }
+    const userData = authDoc.data()
+
+    const clientOK = (req.query.client_id === userData.ClientID);
+    const uriOK = (req.query.redirect_uri === 'https://oauth-redirect.googleusercontent.com/r/myhome-5f414');
+
+    if (clientOK && uriOK) {
+      let redirectAddr = req.query.redirect_uri;
+      redirectAddr += '#access_token=' + userData.AuthToken.toString();
+      redirectAddr += '&token_type=bearer';
+      redirectAddr += '&state=' + requestAnimationFrame.query.state.toString();
+
+      res.redirect(redirectAddr);
+    }
+    else {
+      res.status(404).send({
+        error: 'Invalid client data: ' + req.query.client_id + ', ' + req.query.redirect_uri + '. '
+          + 'Firestore: ' + authDoc.toString() + ', ' + userData.toString()
+      });
+    }
   }
-  const userData = authDoc.data()
-  
-  const clientOK  = (req.query.client_id === userData.ClientID);
-  const uriOK     = (req.query.redirect_uri === 'https://oauth-redirect.googleusercontent.com/r/myhome-5f414');
-  
-  if (clientOK && uriOK){
-    let redirectAddr = req.query.redirect_uri;
-    redirectAddr += '#access_token='+userData.AuthToken.toString();
-    redirectAddr += '&token_type=bearer';
-    redirectAddr += '&state='+requestAnimationFrame.query.state.toString();
-    
-    res.redirect(redirectAddr);
+  catch (e) {
+    res.status(404).send({error: 'Exception thrown: '+e});
   }
-  else{
-    res.status(404).send({error: 'Invalid client data: '+req.query.client_id+', '+req.query.redirect_uri+'. '
-    +'Firestore: '+authDoc.toString()+', '+userData.toString()});
-  }
+
 }
