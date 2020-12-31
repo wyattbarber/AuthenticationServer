eb77ec691e4e9807661a647eef8b4410de28b5a7
wyattbarber <wyattjbarber@gmail.com>
2020-12-31 14:34:27 -0500

    Moved firebase access functions to firebase.ts

---
+++ firestore.ts
@@ -0,0 +1,22 @@
+import * as admin from 'firebase-admin';
+admin.initializeApp();
+const db = admin.firestore();
+const settings = {timestampsInSnapshots: true};
+db.settings(settings);
+
+/**
+ * Asynchronously accesses the authentication document for the given ClientID
+ * 
+ * @param {!express:Request} req 
+ * @param {!express:Response} res 
+ */
+export async function getUserData(req, res): Promise<firebase.firestore.DocumentSnapshot> {
+    const clientDoc = await db.collection('Home Data').doc(req.query.client_id).get();
+    if (clientDoc.exists) {
+        return clientDoc;
+    }
+    else {
+        res.status(404).send({error: 'Could not locate document for this client'});
+        throw 'Error opening document';
+    }
+}
--- index.js
+++ index.js
@@ -1,51 +1,38 @@
+import * as firebase from './firestore';
+
+
 /**
  * Responds to accont linking request from MyHome actions project.
  *
  * @param {!express:Request} req HTTP request context.
  * @param {!express:Response} res HTTP response context.
  */
-
-const Firestore = require('@google-cloud/firestore');
-
 exports.main = (req, res) => {
+  // Load approved user data for given client id
   try {
-    // Load approved user data
-    const firestore = new Firestore({
-      projectId: 'decent-booster-285122',
-    });
-
-    const collection = firestore.collection('Home Data');
-    if (!collection.exists) {
-      res.status(404).send({error: 'Collection not found'});
-      return;
-    }
-    const docRef = collection.doc('Auth');
-    const doc = docRef.get();
-    if (!doc.exists) {
-      res.status(404).send({ error: 'Document not found' });
-      return;
-    }
-    const userData = doc.data()
-
-    const clientOK = (req.query.client_id === userData.ClientID);
-    const uriOK = (req.query.redirect_uri === 'https://oauth-redirect.googleusercontent.com/r/myhome-5f414');
-
-    if (clientOK && uriOK) {
-      let redirectAddr = req.query.redirect_uri;
-      redirectAddr += '#access_token=' + userData.AuthToken.toString();
-      redirectAddr += '&token_type=bearer';
-      redirectAddr += '&state=' + requestAnimationFrame.query.state.toString();
-
-      res.redirect(redirectAddr);
-    }
-    else {
-      res.status(404).send({
-        error: 'Invalid client data: ' + req.query.client_id + ', ' + req.query.redirect_uri + '. '
-          + 'Firestore: ' + doc.toString() + ', ' + userData.toString()
-      });
-    }
+    const userData = firebase.getUserData(req, res);
   }
   catch (e) {
-    res.status(404).send({error: 'Exception thrown: '+e});
+    return;
   }
+
+  // Validate data sent in request
+  const clientOK = (req.query.client_id === userData.ClientID);
+  const uriOK = (req.query.redirect_uri === 'https://oauth-redirect.googleusercontent.com/r/myhome-5f414');
+
+  if (clientOK && uriOK) {
+    let redirectAddr = req.query.redirect_uri;
+    redirectAddr += '#access_token=' + userData.AuthToken.toString();
+    redirectAddr += '&token_type=bearer';
+    redirectAddr += '&state=' + requestAnimationFrame.query.state.toString();
+
+    res.redirect(redirectAddr);
+  }
+  else {
+    res.status(404).send({
+      error: 'Invalid client data: ' + req.query.client_id + ', ' + req.query.redirect_uri + '. '
+        + 'Firestore: ' + doc.toString() + ', ' + userData.toString()
+    });
+  }
+
 }
