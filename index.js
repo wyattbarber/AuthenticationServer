3cd0095674a60e0b51b4da173a871bb5513d9d5f
wyattbarber <wyattjbarber@gmail.com>
2020-12-31 19:01:10 -0500

    Authorization working, renamed and added empty function token

--- index.js
+++ index.js
@@ -7,36 +7,42 @@
 
 firestore = require('./firestore');
 
-exports.main = (req, res) => {
-  // Load approved user data for given client id
-  let userData;
-  let AuthToken;
-  try {
-    userData = firestore.getUserData(req, res);
-    AuthToken = userData.AuthToken;
-  }
-  catch (e) {
-    return;
-  }
-
-  // Validate data sent in request
-  try {
-    const uriOK = (req.query.redirect_uri === 'https://oauth-redirect.googleusercontent.com/r/myhome-5f414');
-    if (uriOK) {
-      let redirectAddr = req.query.redirect_uri;
-      redirectAddr += '#access_token=' + AuthToken;
-      redirectAddr += '&token_type=bearer';
-      redirectAddr += '&state=' + req.query.state;
-
-      res.redirect(redirectAddr);
+exports = {
+  authorize: function (req, res) {
+    // Load approved user data for given client id
+    let userData;
+    let AuthToken;
+    try {
+      userData = firestore.getUserData(req, res);
+      AuthToken = userData.AuthToken;
     }
-    else {
-      res.status(404).send({
-        error: 'Invalid client data: ' + req.query.client_id + ', ' + req.query.redirect_uri});
+    catch (e) {
+      return;
     }
-  }
-  catch (e) {
-    res.status(404).send({error: 'Exception thrown: '+e});
-  }
 
+    // Validate data sent in request
+    try {
+      const uriOK = (req.query.redirect_uri === 'https://oauth-redirect.googleusercontent.com/r/myhome-5f414');
+      if (uriOK) {
+        let redirectAddr = req.query.redirect_uri;
+        redirectAddr += '#access_token=' + AuthToken;
+        redirectAddr += '&token_type=bearer';
+        redirectAddr += '&state=' + req.query.state;
+
+        res.redirect(redirectAddr);
+      }
+      else {
+        res.status(404).send({
+          error: 'Invalid client data: ' + req.query.client_id + ', ' + req.query.redirect_uri
+        });
+      }
+    }
+    catch (e) {
+      res.status(404).send({ error: 'Exception thrown: ' + e });
+    }
+  },
+
+  token: function (req, res) {
+    
+  }
 }
