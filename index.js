0953c38243b7e6108f83b4c637319045723bd0a2
wyattbarber <wyattjbarber@gmail.com>
2020-12-31 19:06:46 -0500

    Trying again

--- index.js
+++ index.js
@@ -7,42 +7,40 @@
 
 firestore = require('./firestore');
 
-exports = {
-  authorize: function (req, res) {
-    // Load approved user data for given client id
-    let userData;
-    let AuthToken;
-    try {
-      userData = firestore.getUserData(req, res);
-      AuthToken = userData.AuthToken;
-    }
-    catch (e) {
-      return;
-    }
-
-    // Validate data sent in request
-    try {
-      const uriOK = (req.query.redirect_uri === 'https://oauth-redirect.googleusercontent.com/r/myhome-5f414');
-      if (uriOK) {
-        let redirectAddr = req.query.redirect_uri;
-        redirectAddr += '#access_token=' + AuthToken;
-        redirectAddr += '&token_type=bearer';
-        redirectAddr += '&state=' + req.query.state;
-
-        res.redirect(redirectAddr);
-      }
-      else {
-        res.status(404).send({
-          error: 'Invalid client data: ' + req.query.client_id + ', ' + req.query.redirect_uri
-        });
-      }
-    }
-    catch (e) {
-      res.status(404).send({ error: 'Exception thrown: ' + e });
-    }
-  },
-
-  token: function (req, res) {
-    
+exports.authorize = (req, res) => {
+  // Load approved user data for given client id
+  let userData;
+  let AuthToken;
+  try {
+    userData = firestore.getUserData(req, res);
+    AuthToken = userData.AuthToken;
   }
+  catch (e) {
+    return;
+  }
+
+  // Validate data sent in request
+  try {
+    const uriOK = (req.query.redirect_uri === 'https://oauth-redirect.googleusercontent.com/r/myhome-5f414');
+    if (uriOK) {
+      let redirectAddr = req.query.redirect_uri;
+      redirectAddr += '#access_token=' + AuthToken;
+      redirectAddr += '&token_type=bearer';
+      redirectAddr += '&state=' + req.query.state;
+
+      res.redirect(redirectAddr);
+    }
+    else {
+      res.status(404).send({
+        error: 'Invalid client data: ' + req.query.client_id + ', ' + req.query.redirect_uri
+      });
+    }
+  }
+  catch (e) {
+    res.status(404).send({ error: 'Exception thrown: ' + e });
+  }
+}
+
+exports.token = (req, res) => {
+
 }
